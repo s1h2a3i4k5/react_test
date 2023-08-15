b840b1cf298a39c7e5b6bcf5bda7b6485ba9eb72
s1h2a3i4k5 <ahamedunnisah786@gmail.com>
2023-08-15 09:11:13 +0530

    adding

---
+++ app.js
@@ -0,0 +1,7 @@
+const h1tag = React.createElement("h1",{},"Welcome to React js");
+console.log(h1tag);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(h1tag);
+// const h1tag = React.createElement("div",{ id : "parent"},
