cd7d9f2a2e90417facf2328a855f1f69752650ae
s1h2a3i4k5 <ahamedunnisah786@gmail.com>
2023-08-16 09:10:59 +0530

    New update

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const h1tag = React.createElement("h1",{},"Welcome to React js");
+import React from "react";
+import  ReactDOM from "react-dom";
+
+// const h1tag = React.createElement("h1",{},"Welcome to React js");
+// console.log(h1tag);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(h1tag);
+// // const h1tag = React.createElement("div",{ id : "parent"},
+
+const h1tag = React.createElement("div",{ id : "parent"},
+
+React.createElement("div",{ id : "child"}),
+
+React.createElement("h1",{ id : "some"}, "Hello All, I am from Reactjs and NestedH1")
+);
 console.log(h1tag);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(h1tag);
-// const h1tag = React.createElement("div",{ id : "parent"},
